503a217fa3a19f6ccdf752128ccf46b8189a1d1e
norahl73 <mbv7xs@virginia.edu>
2025-07-22 02:29:19 -0400

    adding homepage basic text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,32 @@
 const Home = () => {
-    return <h1>Welcome to the Home Page</h1>;
+  return (
+    <>
+      <h1>Welcome to the Home Page</h1>
+      <p>Who are we?</p>
+      <div className="about">
+        <h2>Who we are...</h2>
+        <p>Insert description here...</p>
+        <h3>Become a member today!</h3>
+        <h3>Follow us on our Socials!</h3>
+      </div>
+      <div className="ezcard">
+        <h2>How to use our EZCards</h2>
+        <p>Insert Description Here....</p>
+      </div>
+      <div className="newsletter">
+        <h2>Previous Newsletters</h2>
+        <p>Blurb...</p>
+        <p>Caption..</p>
+        <p>TSA News...</p>
+      </div>
+      <div className="currentEvents">
+        <h2>Current Events</h2>
+        <p>Big news..</p>
+        <p>More news..</p>
+      </div>
+      <div className="footer"></div>
+    </>
+  );
 };
 
 export default Home;
