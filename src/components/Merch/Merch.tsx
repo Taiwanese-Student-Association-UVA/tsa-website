4fec9cbfd0a2523f2c739a741860ad8a22e772c6
Hannah Duong <hannah.n.duong@gmail.com>
2026-01-03 19:44:04 -0500

    shady hero added

--- src/components/Merch/Merch.tsx
+++ src/components/Merch/Merch.tsx
@@ -1,5 +1,22 @@
+import styles from './Merch.module.css';
+
 const Merch = () => {
-    return <h1>Merch Page</h1>;
+  return (
+    <div className={styles['merch-container']}> 
+      {/* Note: Use brackets styles['merch-hero'] if your CSS class has a hyphen */}
+      <section className={styles['merch-hero']}>
+        <div className={styles['hero-content']}>
+          <h1>TSA Merchandise</h1>
+          <p>TSA is Cool </p>
+        </div>
+      </section>
+
+      {/* Grid for your cards starts here */}
+      <div className={styles['merch-grid']}>
+        {/* We can add the cards here next */}
+      </div>
+    </div>
+  );
 };
 
 export default Merch;
