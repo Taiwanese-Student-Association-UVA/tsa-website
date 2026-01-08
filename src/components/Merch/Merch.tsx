d12f4507afe19f42dc91edd0917829b31c5a948e
Hannah Duong <hannah.n.duong@gmail.com>
2026-01-07 20:18:53 -0500

    finished card header draft..

--- src/components/Merch/Merch.tsx
+++ src/components/Merch/Merch.tsx
@@ -61,22 +61,29 @@ const Merch = () => {
             transition={{ duration: 1.5, ease: [0.45, 0, 0.55, 1] }}
             viewport={{ once: false, amount: 0.1 }}
           >
-            {/* NIGHT MARKET SHIRT */}
+            {/* NIGHT MARKET SHIRT -----------------------------------------------------------------*/}
             <div className={styles['merch-card']} style={{ backgroundColor: '#FFF5D4' }}>
+
+              {/* MASK */}
               <img 
                   src="/MerchPhotos/NightMarket/2023-2024/NM_23-24_BACK.PNG" 
                   className={styles['nm-background']} 
                   alt="Background"
               />
+
+              {/* HEADERS */}
               <div className={styles['card-content']}>
                   <img src="/MerchPhotos/NightMarket/2023-2024/NM_23-24_FRONT.PNG" className={styles.logo} alt="logo"/>
                   <h2>Night Market Shirts</h2>
                   <button className={styles['buy-btn']}>Buy Now</button>
               </div>
+
+              {/* IMAGE */}
               <div className={styles['card-image-wrapper']}>
                   <img src="/MerchPhotos/NightMarket/2023-2024/NM_23-24_BACK.PNG" alt="Product"/>
               </div>
             </div>
+            
             
             {/* YINGHUA SHIRT */}
             <div className={styles['merch-card']} style={{ backgroundColor: '#6746C1' }}>
@@ -100,14 +107,14 @@ const Merch = () => {
                 </button>
               </div>
 
-              {/* Wrap the image so it anchors to the right correctly */}
+              {/* IMAGE */}
               <div 
                 className={styles['card-image-wrapper']} 
                 style={{ 
                   background: 'linear-gradient(to bottom, #E7DEFF 0%, #CAB6FF 50%, #9578E6 100%)' 
                 }}
               >
-                <img src="Mer.jpg" alt="Merch" />
+                <img src="/MerchPhotos/Yinghua/2024-2025/yinghua_back.png" alt="Merch" />
               </div>
             </div>
 
@@ -115,19 +122,32 @@ const Merch = () => {
 
 
             {/* GENBOD SHIRT */}
-            <div className={styles['merch-card']} style ={{backgroundColor: '#FEFCC5'} }>
+            <div className={styles['merch-card']} style ={{backgroundColor: '#A8BFFF'} }>
               <img 
                   src="\MerchPhotos\GenBod\2024-2025\Background.png" 
                   className={styles['genbod-background']} 
                   alt="Background"
               />
+
               <div className={styles['card-content']}>
-                <img src="/MerchPhotos/GenBod/2024-2025/GoldLogo.png" className={styles.logo} alt="logo"/>
+                <img src="/MerchPhotos/GenBod/2024-2025/Logo.png" className={styles.logo} alt="logo"/>
                 <h2 style={{ color: '#444343ff' }}>Genbod Shirts</h2>
                 <button className={styles['buy-btn']} style={{backgroundColor: '#444343ff' }}>Buy Now</button>
               </div>
-              <div className={styles['card-image-wrapper']} style={{backgroundColor: '#FFFEEC'}}>
-                <img src="Mer.jpg" alt="Merch" />
+              <div className={styles['card-image-wrapper']} style={
+                  {
+                    // backgroundColor: '#bfcefaff',
+                    
+                    backgroundColor: '#c6d4ffe1', 
+                    backgroundImage: `
+                      /* Using #cddbff for a brighter, crisper blue line */
+                      repeating-linear-gradient(0deg, #cddbff21, #cddbff21 35px, transparent 35px, transparent 70px),
+                      repeating-linear-gradient(90deg, #cddbff25, #cddbff25 35px, transparent 35px, transparent 70px)
+                    `,
+                    backgroundBlendMode: 'screen'
+                  }
+                }>
+                <img src="\MerchPhotos\GenBod\2024-2025\Genbod_Back.png" alt="Merch" style={{filter: 'brightness(0) invert(1)', zIndex: 2}} />
               </div>
             </div>
 
