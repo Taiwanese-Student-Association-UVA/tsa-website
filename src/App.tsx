1c53d3b94649a844e035268b541ccf0d82ff7921
BrandonYuan01 <spmsby11@gmail.com>
2026-03-15 14:53:30 -0400

    suttff

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Sponsors from "./components/Sponsors/Sponsors";
 import Archive from "./components/Archive/Archive";
 import Newsletters from "./components/Archive/Newsletters/Newsletters";
 import Merch from "./components/Merch/Merch";
-// import { EventPopup } from "./components/Nightmarket/EventPopup";
+import { EventPopup } from "./components/Nightmarket/EventPopup";
 import Games from "./components/Games/Games";
 import MobileHeader from "./components/Header/MobileHeader";
 import useIsMobile from "./utils/useIsMobile";
@@ -17,17 +17,17 @@ import "./App.css";
 
 const App = () => {
   const isMobile = useIsMobile();
-    // const eventDate = new Date("2026-04-04T12:00:00-04:00");
+    const eventDate = new Date("2026-04-04T12:00:00-04:00");
 
 
   return (
     <Router>
       {!isMobile && <Header />}
       {isMobile && <MobileHeader />}
-      {/*<EventPopup*/}
-      {/*    targetDate={eventDate}*/}
-      {/*    eventLink="https://your-event-page.com" // change link to NM site*/}
-      {/*/>*/}
+      <EventPopup
+          targetDate={eventDate}
+          eventLink="https://your-event-page.com" // change link to NM site
+      />
 
       <div className={isMobile ? "contentWithMobileHeader" : "content"}>
         <Routes>
